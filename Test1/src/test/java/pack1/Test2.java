6108d47d2c47c1155279f36bd00e626c307024ec
Syg7498 <86937894+Syg7498@users.noreply.github.com>
2025-03-12 20:50:25 +0530

    Update Test2.java

--- Test1/src/test/java/pack1/Test2.java
+++ Test1/src/test/java/pack1/Test2.java
@@ -3,5 +3,11 @@ package pack1;
 public class Test2 {
 public static void main(String[] args) {
 	System.out.println("Test2");
+
+	//updated by another user
+	System.out.println("Test2");
+	System.out.println("Test2");
+	System.out.println("Test2");
+	System.out.println("Test2");
 }
 }
