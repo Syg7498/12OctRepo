6fee0ebe8fe6d97a70d1ac42832af5a7ca514866
SUYOG YEOLE <SUYOG YEOLE@DESKTOP-C59MQFQ>
2025-03-13 19:57:52 +0530

    Test Class

--- Test1/src/test/java/pack1/Test2.java
+++ Test1/src/test/java/pack1/Test2.java
@@ -1,17 +1,45 @@
 package pack1;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class Test2 {
-	public static void main(String[] args) {
-
+	@Test
+	void m1() {
 		System.out.println();
-
+		System.out.println("Test2 m1");
+		// updated by another user
 		System.out.println("Test2");
+	}
 
+	@Test
+	void m2() {
+		System.out.println();
+		System.out.println("Test2 m2");
+		// updated by another user
+		System.out.println("Test2");
+		Assert.fail();
+	}
+
+	@Test
+	void m3() {
+		System.out.println();
+		System.out.println("Test2 m3");
 		// updated by another user
 		System.out.println("Test2");
 		System.out.println("Test2");
 		System.out.println("Test2");
 		System.out.println("Test2");
-
+	}
+	
+	@Test(dependsOnMethods = "m2")
+	void m4() {
+		System.out.println();
+		System.out.println("Test2 m3");
+		// updated by another user
+		System.out.println("Test2");
+		System.out.println("Test2");
+		System.out.println("Test2");
+		System.out.println("Test2");
 	}
 }
